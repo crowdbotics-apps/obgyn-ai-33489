9f3989a3c92698dedb1f7c00653a0325f7af1f30
Crowdbotics <team@crowdbotics.com>
2022-02-10 04:58:54 +0000

    Initial commit.

---
+++ backend/obgyn_ai_33489/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for obgyn_ai_33489 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'obgyn_ai_33489.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "obgyn_ai_33489",
+  url: "https://obgyn_ai_33489.botics.co",
+  api: "https://obgyn_ai_33489.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
